61c200294895d751cec1fda7c636d1791fd130a6
franco <francogrimoldidelgado@gmail.com>
2026-01-15 22:13:48 -0300

    cambios en el wpp

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 // Editá SOLO este archivo para configurar tus datos de contacto
 window.RF_CONFIG = {
-phone: '5491165028573', // ← cambiá por tu número (sin +), ej: 5491151234567
+phone: '5491153112502', // ← cambiá por tu número (sin +), ej: 5491151234567
 businessName: 'RF Repuestos'
 };
 
